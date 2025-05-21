8687dea6e87742b90a9dc1e49d4227a7f5be2be2
unknown <me.hasnainkamal@gmail.com>
2025-05-20 21:09:47 -0700

    1st commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// ADDITION
+var fNumber = +prompt("Enter Your First Number To add");
+var sNumber = +prompt("Enter YOur Second Number");
+alert(fNumber + sNumber);
+
+// SUBSTRUCTION
+var fNumber = +prompt("Enter Your First Number To substruct");
+var sNumber = +prompt("Enter YOur Second Number");
+alert(fNumber - sNumber);
+
+// MULTIPLICATION
+var fNumber = +prompt("Enter Your First Number To multiply");
+var sNumber = +prompt("Enter YOur Second Number");
+alert(fNumber * sNumber);
+
+// DIVISION
+var fNumber = +prompt("Enter Your First Number To divide");
+var sNumber = +prompt("Enter YOur Second Number");
+alert(fNumber / sNumber);
+
+
+alert("Thank You For Using This");
